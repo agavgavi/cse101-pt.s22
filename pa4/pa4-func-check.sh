8aa14b97dc0e23f0fcc30f7763511dc3f01eeef7
Andrew Gavgavian <andrewgee7@gmail.com>
2022-05-23 19:52:00 -0700

    better timeout

--- pa4/pa4-func-check.sh
+++ pa4/pa4-func-check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-SRCDIR=https://raw.githubusercontent.com/agavgavi/cse101-pt.s22/master/pa4
+SRCDIR=https://raw.githubusercontent.com/agavgavi/s22/master/pa4
 NUMTESTS=5
 PNTSPERTEST=2
 let MAXPTS=$NUMTESTS*$PNTSPERTEST
@@ -37,19 +37,28 @@ read verbose
 for NUM in $(seq 1 $NUMTESTS); do
   rm -f outfile$NUM.txt
   let RUNTIME=$TIME
+  let MAXRUNTIME=$RUNTIME*3
   if [ $NUM -eq 4 ]; then
     let RUNTIME=3*$TIME
+    let MAXRUNTIME=$RUNTIME*3
   fi
-  timeout $RUNTIME ./Sparse infile$NUM.txt outfile$NUM.txt &> garbage >> garbage #all stdout/stderr thrown away
-  if [ $? -eq 124 ]; then
+  timeout $MAXRUNTIME /usr/bin/time -po time$NUM.txt ./Sparse infile$NUM.txt outfile$NUM.txt &> garbage
+  let timedOut=0
+  if [ $timedOut -eq 1 ]; then
+    let timedOut=1
     echo -e "${RED} SPARSE TEST TIMED OUT ${NC}"
   fi
+  userTime=`perl -ane 'print $F[1] if $F[0] eq "user"' time$NUM.txt`
+  tooSlow=$(echo "$userTime > $RUNTIME" |bc -l)
   diff -bBwu outfile$NUM.txt model-outfile$NUM.txt > diff$NUM.txt &>> diff$NUM.txt
   echo "Sparse Test $NUM:"
   echo "=========="
   cat diff$NUM.txt
+  if (( $tooSlow )); then
+    echo -e "${RED}SPARSE TEST TIMED OUT (Slower than $MAXTIME) ${NC}"
+  fi
   echo "=========="
-  if [ -e diff$NUM.txt ] && [[ ! -s diff$NUM.txt ]]; then # increment number of tests passed counter
+  if [ -e diff$NUM.txt ] && [[ ! -s diff$NUM.txt ]] && [[ $tooSlow -eq 0 ]] && [[ $timedOut -eq 0 ]]; then # increment number of tests passed counter
     let pathtestspassed+=1
   fi
 done
@@ -62,9 +71,10 @@ if [ "$pathtestspoints" -gt "$MAXPTS" ]; then # max 10 points
   let pathtestspoints=$(expr $MAXPTS)
 fi
 echo "Passed $pathtestspassed Sparse tests for a total of $pathtestspoints / $MAXPTS points"
-
 echo ""
 echo ""
+echo "Now doing valgrind checks"
+read verbose
 timeout $TIME valgrind --leak-check=full -v ./Sparse infile2.txt outfile2.txt > garbage 2> Sparse-mem.txt
 
 cat Sparse-mem.txt
